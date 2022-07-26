02a61e2a34facadaae6434e36da2661ed4d4e2ef
Aral Roca <aral-rg@hotmail.com>
2022-07-26 19:24:56 +0200

    Modify text

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ export default function Home() {
     <div className="homepage">
       <h1 style={{ fontSize: 20 }}>Bienvenidos,</h1>
       <p>
-        Mi nombre es Bárbara Sánchez y estáis en mi territorio. Inicio una
-        aventura con este blog, una que da un poco de miedo pero también que
-        emociona. Quería un lugar en el que volcar el batiburrillo que tengo en
-        la cabeza y aquí lo tenéis. Mis relatos y poemas los elaboro con mucho
-        cariño y espero que algo de ese afecto y dedicación os llegue.
-        Aventuras, misterios, sorpresas o días del montón… ni confirmo, ni
-        desmiento lo que os encontraréis al cruzar el umbral. Bienvenidos a mi
-        mente, entrad bajo vuestra responsabilidad. No se admiten devoluciones.
+        Soy B. Sánchez y estáis en mi territorio. Inicio una aventura con este
+        blog, una que da un poco de miedo pero también que emociona. Quería un
+        lugar en el que volcar el batiburrillo que tengo en la cabeza y aquí lo
+        tenéis. Mis relatos y poemas los elaboro con mucho cariño y espero que
+        algo de ese afecto y dedicación os llegue. Aventuras, misterios,
+        sorpresas o días del montón… ni confirmo, ni desmiento lo que os
+        encontraréis al cruzar el umbral. Bienvenidos a mi mente, entrad bajo
+        vuestra responsabilidad. No se admiten devoluciones.
       </p>
     </div>
   )
